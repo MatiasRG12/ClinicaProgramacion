7e9bffbdcc38701a969fda7d104a261551f3cc25
matias <matias.ramirezg@uqvirtual.edu.co>
2023-10-17 17:30:44 -0500

    correcion dependencia seguridad

--- src/test/resources/dataset.sql
+++ src/test/resources/dataset.sql
@@ -4,5 +4,5 @@ insert into usuario values ("medico","987654321","234","correo2@correo.com");
 insert into persona values ("123456789","Armenia","foto","Pepe","3225205639","123456789");
 insert into persona values ("987654321","Pereira","foto2","Pepe2","3225205638","987654321");
 
-insert into medico values (1,2,3,"123456789");
-insert into medico values (1,2,3,"987654321");
+insert into medico values (0,0,1,"123456789");
+insert into medico values (1,0,1,"987654321");
